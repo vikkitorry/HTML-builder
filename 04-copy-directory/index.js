097c5ed1a5de9035a404c4d735fb1e90dac13643
vikkitorry <vikkitorry6716@gmail.com>
2023-05-03 16:39:05 +1000

    feat: add solution for task 03

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,11 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true }, err => {
+  if (err) throw err;
+});
+
+fs.readdir(path.join(__dirname, 'files'), {recursive: true }, (err, elm) => {
+  if (err) throw err;
+  for (let i = 0; i < elm.length; i++)
+});
