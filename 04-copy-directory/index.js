211c6bc26aef2adea2f08653e8e4d7c7b9339a7d
vikkitorry <vikkitorry6716@gmail.com>
2023-05-03 17:28:59 +1000

    feat: add solution for task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,31 @@
 const fs = require('fs');
 const path = require('path');
+const folder = path.join(__dirname, 'files');
+const newFolder = path.join(__dirname, 'files-copy');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true }, err => {
+fs.mkdir(newFolder, {recursive: true }, (err) => {
   if (err) throw err;
 });
 
-fs.readdir(path.join(__dirname, 'files'), {recursive: true }, (err, elm) => {
+fs.readdir(folder, (err, files) => {
   if (err) throw err;
-  for (let i = 0; i < elm.length; i++)
+  for (let i = 0; i < files.length; i++) {
+    fs.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]), (err) => {
+      if (err) throw err;
+    });
+  }
+
+  fs.readdir(newFolder, (err, filesCopied) => {
+    if (err) throw err;
+    if (files.length < filesCopied.length) {
+      const uniq  = new Set(files);
+      // find extra file in new folder and delete
+      const delExtra = filesCopied.filter((elm) => !uniq.includes(elm));
+      for (let i = 0; i < delExtra.length; i++) {
+        fs.unlink(path.join(__dirname, 'files-copy', delExtra[i]), (err) => {
+          if (err) throw err;
+        });
+      }
+    }
+  });
 });
