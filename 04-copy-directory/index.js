3a15219c71f4d666a66dcce97d76c675ff8fb132
vikkitorry <vikkitorry6716@gmail.com>
2023-05-10 14:20:33 +1000

    refactor: remove bug

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,18 +3,18 @@ const path = require('path');
 const source = path.join(__dirname, 'files');
 const dest = path.join(__dirname, 'files-copy');
 
-
 const copyFolder = async () => {
-    await fs.promises.mkdir(dest, {recursive: true});
-    const files = await fs.promises.readdir(dest);
-    for (let i = 0; i < files.length; i++) {
-        await fs.promises.rm(path.resolve(__dirname, 'files-copy', files[i]))
-    };
-    const docs = await fs.promises.readdir(source);
-    for (let i = 0; i < docs.length; i++) {
-        const fileSource = path.resolve(__dirname, 'files', docs[i]);
-        const fileDest = path.resolve(__dirname, 'files-copy', docs[i]);
-        await fs.promises.copyFile(fileSource, fileDest);
-    }
-}
+  await fs.promises.mkdir(dest, {recursive: true});
+  const files = await fs.promises.readdir(dest);
+  for (let i = 0; i < files.length; i++) {
+    const fileCopy = path.join(__dirname, 'files-copy', files[i]);
+    await fs.promises.rm(fileCopy);
+  };
+  const docs = await fs.promises.readdir(source);
+  for (let i = 0; i < docs.length; i++) {
+    const fileDest = path.join(__dirname, 'files-copy', docs[i]);
+    const fileSource = path.join(__dirname, 'files', docs[i]);
+    await fs.promises.copyFile(fileSource, fileDest);
+  }
+};
 copyFolder();
