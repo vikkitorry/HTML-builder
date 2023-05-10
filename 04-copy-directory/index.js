8a75d9a18971ba72da75b192aa7bab9dbb6c13aa
vikkitorry <vikkitorry6716@gmail.com>
2023-05-10 14:12:07 +1000

    rafactor: remove bugs

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,29 +3,18 @@ const path = require('path');
 const source = path.join(__dirname, 'files');
 const dest = path.join(__dirname, 'files-copy');
 
-fs.mkdir(dest, {recursive: true }, (err) => {
-  if (err) throw err;
-});
 
-fs.readdir(source, (err, files) => {
-  if (err) throw err;
-  for (let i = 0; i < files.length; i++) {
-    fs.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]), (err) => {
-      if (err) throw err;
-    });
-  }
-
-  fs.readdir(dest, (err, filesCopied) => {
-    if (err) throw err;
-    if (files.length < filesCopied.length) {
-      const uniq  = new Set(files);
-      // find extra file in new folder and delete
-      const delExtra = filesCopied.filter((elm) => !uniq.includes(elm));
-      for (let i = 0; i < delExtra.length; i++) {
-        fs.unlink(path.join(__dirname, 'files-copy', delExtra[i]), (err) => {
-          if (err) throw err;
-        });
-      }
+const copyFolder = async () => {
+    await fs.promises.mkdir(dest, {recursive: true});
+    const files = await fs.promises.readdir(dest);
+    for (let i = 0; i < files.length; i++) {
+        await fs.promises.rm(path.resolve(__dirname, 'files-copy', files[i]))
+    };
+    const docs = await fs.promises.readdir(source);
+    for (let i = 0; i < docs.length; i++) {
+        const fileSource = path.resolve(__dirname, 'files', docs[i]);
+        const fileDest = path.resolve(__dirname, 'files-copy', docs[i]);
+        await fs.promises.copyFile(fileSource, fileDest);
     }
-  });
-});
+}
+copyFolder();
